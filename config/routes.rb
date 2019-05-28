22664bd366b99f388367ef6e3aa280495cf99f97
Daniel Railsback <Danielrailsbackgithub@gmail.com>
2019-05-28 23:44:57 +0000

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   devise_for :users, controllers: { registrations: 'users/registrations'}
+  resources :users do
+    resource :profile
+  end  
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   # This creates custom URL and displays it to application.html.erb as new_contact_path 
