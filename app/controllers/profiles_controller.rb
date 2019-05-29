469b4071d5cde40606cf8a5d9c2b6241a5bcbe6e
Daniel Railsback <Danielrailsbackgithub@gmail.com>
2019-05-29 05:28:16 +0000

    added user profiles form.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,5 +4,6 @@ class ProfilesController < ApplicationController
   # Get to /user/:user_id/profile/new
   def new
     # Render blank profile details form
+    @profile = Profile.new
   end  
 end    
